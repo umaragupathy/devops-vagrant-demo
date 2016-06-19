af71c1ee129ff44470376c8363cbf9d37e5475e2
u.ragupathy@unsw.edu.au <u.ragupathy@unsw.edu.au>
2016-06-19 12:19:39 +1000

    updated git ignore

---
+++ .chef/knife.rb
@@ -0,0 +1,5 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # devops-vagrant-demo
+
